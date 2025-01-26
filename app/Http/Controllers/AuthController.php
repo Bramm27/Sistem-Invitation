fee8987e97e73503a7bd71f4695b9d033724e228
Bramm <bramm.school17f@gmail.com>
2025-01-26 19:29:42 +0700

    refactor: remove login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,21 +13,6 @@ class AuthController extends Controller
 
     public function loginPost(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:6',
-        ]);
-    
-        $credentials = $request->only('email', 'password');
-    
-        // Debug kredensial
-        // dd($credentials);
-    
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            dd('Login berhasil');
-        }
-    
-        dd('Login gagal');
+
     }
 }
